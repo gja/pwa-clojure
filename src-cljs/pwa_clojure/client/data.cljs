4a4be34853d0432be33b844c692a21a3612b1675
Tejas Dinkar <tejas@gja.in>
2016-11-15 22:06:00 +0530

    Added a cached get

--- src-cljs/pwa_clojure/client/data.cljs
+++ src-cljs/pwa_clojure/client/data.cljs
@@ -3,9 +3,22 @@
             [bidi.bidi :as bidi]
             [ajax.core :as ajax]))
 
+(defn- do-get [route callback]
+  (ajax/GET route
+            {:response-format :json
+             :keywords? true
+             :handler callback}))
+
+(defn- cached-get [route callback]
+  (if js/window.caches
+    (-> js/window.caches
+        (.match route)
+        (.then (fn [response]
+                 (if response
+                   (-> response .json (js->clj {:keywordize-keys true}) (.then callback))
+                   (do-get route callback)))))
+    (do-get route callback)))
+
 (defn load-data [handler args callback]
   (let [route (apply bidi/path-for routes/api-routes handler (flatten (vec args)))]
-    (ajax/GET route
-              {:response-format :json
-               :keywords? true
-               :handler callback})))
+    (cached-get route callback)))
