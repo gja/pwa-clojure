63fbbc08ae853b08fef749de8af32bb5a27c55a2
Tejas Dinkar <tejas@gja.in>
2016-11-15 23:35:11 +0530

    The Service Worker Purges Old Caches

--- src-svc/pwa_clojure/service_worker.cljs
+++ src-svc/pwa_clojure/service_worker.cljs
@@ -7,17 +7,27 @@
 (def app-cache-name "pwa-clojure-app")
 (def data-cache-name "pwa-clojure-data")
 
+(defn- purge-old-caches [e]
+  (-> js/caches
+      .keys
+      (.then (fn [keys]
+               (->> keys
+                    (map #(when-not (contains? #{app-cache-name data-cache-name} %)
+                            (.delete js/caches %)))
+                    clj->js
+                    js/Promise.all)))))
+
 (def files-to-cache ["/js/main.js"
                      "/shell.html"])
 
+
 (defn- install-service-worker [e]
   (js/console.log "[ServiceWorker] Installing")
-  (js/e.waitUntil
-   (-> js/caches
-       (.open app-cache-name)
-       (.then (fn [cache]
-                (js/console.log "[ServiceWorker] Caching Shell")
-                (.addAll cache (clj->js files-to-cache)))))))
+  (-> js/caches
+      (.open app-cache-name)
+      (.then (fn [cache]
+               (js/console.log "[ServiceWorker] Caching Shell")
+               (.addAll cache (clj->js files-to-cache))))))
 
 (defn- return-shell [e]
   (js/console.log "[ServiceWorker] Cached Page")
@@ -36,17 +46,16 @@
 
 (defn- fetch-event [e]
   (js/console.log "[ServiceWorker] Fetch" (-> e .-request .-url))
-  (.respondWith
-   e
-   (let [url (-> e .-request .-url url/url)]
-     (case (:host url)
-       ("localhost" "pwa-clojure.staging.quintype.io")
-       (fetch-cached (:path url) e)
+  (let [url (-> e .-request .-url url/url)]
+    (case (:host url)
+      ("localhost" "pwa-clojure.staging.quintype.io")
+      (fetch-cached (:path url) e)
 
-       "my-images.net"
-       (comment "Yes, you can cache images from other domains too!")
+      "my-images.net"
+      (comment "Yes, you can cache images from other domains too!")
 
-       (js/fetch (.-request e))))))
+      (js/fetch (.-request e)))))
 
-(.addEventListener js/self "install" install-service-worker)
-(.addEventListener js/self "fetch" fetch-event)
+(.addEventListener js/self "install" #(.waitUntil % (install-service-worker %)))
+(.addEventListener js/self "fetch" #(.respondWith % (fetch-event %)))
+(.addEventListener js/self "activate" #(.waitUntil % (purge-old-caches %)))
