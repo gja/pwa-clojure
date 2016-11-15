b7ae7a660735aebf4efa510a099c698ed6c26c17
Tejas Dinkar <tejas@gja.in>
2016-11-16 00:53:14 +0530

    A bit of cleanup of stuff

--- src-cljs/pwa_clojure/client/data.cljs
+++ src-cljs/pwa_clojure/client/data.cljs
@@ -11,11 +11,8 @@
              :keywords? true
              :handler callback}))
 
-(defn- fetch-callback [response callback]
-  (-> response
-      .json
-      (.then #(js->clj % :keywordize-keys true))
-      (.then callback)))
+(defn- response->clj [response]
+  (-> response .json (.then #(js->clj % :keywordize-keys true))))
 
 (defn- cache-response [route response]
   (js/console.log "Caching Data" route)
@@ -24,20 +21,20 @@
         (.open data-cache-name)
         (.then #(.put % route clone)))))
 
-(defn- cached-get [route callback]
+(defn- cached-get [route]
   (-> (.match js/window.caches route)
       (.then
        (fn [response]
          (if response
-           (fetch-callback response callback)
+           (response->clj response)
            (-> (js/fetch route)
                (.then
                 (fn [response]
                   (cache-response route response)
-                  (fetch-callback response callback)))))))))
+                  (response->clj response)))))))))
 
 (defn load-data [handler args callback]
   (let [route (apply bidi/path-for routes/api-routes handler (flatten (vec args)))]
     (if (and js/window.caches js/fetch)
-      (cached-get route callback)
+      (-> (cached-get route) (.then callback))
       (fallback-get route callback))))
