b004df04f8266fc969012810657f1a594cb80846
Tejas Dinkar <tejas@gja.in>
2016-11-18 08:30:36 +0530

    Adding a PWA manifest

--- src-clj/pwa_clojure/views.clj
+++ src-clj/pwa_clojure/views.clj
@@ -12,6 +12,7 @@
     [:head
      [:title title]
      [:link {:rel "stylesheet" :href "/css/main.css"}]
+     [:link {:rel "manifest" :href "/manifest.json"}]
      (seq seo-fields)
      [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Cardo:400,700,400italic|Open+Sans:400,800"}]
      [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
